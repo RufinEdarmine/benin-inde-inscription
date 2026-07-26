741aa1fc67a9336514c44110c7998feb858b161f
Rufin Edarmine <rufinedarmine2001@gmail.com>
2026-07-26 10:11:49 +0530

    Retrait du # dans les URLs (BrowserRouter)

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,14 @@
-import { HashRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Inscription from "./pages/Inscription";
 import Admin from "./pages/Admin";
 
 export default function App() {
   return (
-    <HashRouter>
+    <BrowserRouter>
       <Routes>
         <Route path="/" element={<Inscription />} />
         <Route path="/admin" element={<Admin />} />
       </Routes>
-    </HashRouter>
+    </BrowserRouter>
   );
 }
